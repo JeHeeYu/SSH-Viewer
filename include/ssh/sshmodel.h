08e20f1efb49fe586c141809f9f610884e907a6e
JeHeeYu <sees1234558@gmail.com>
2025-01-01 17:12:32 +0900

    feat: Add SSH connection code

--- include/ssh/sshmodel.h
+++ include/ssh/sshmodel.h
@@ -7,18 +7,19 @@
 
 #include "include/singleton.h"
 #include "ssh/sshdata.h"
+#include "ssh/sshmanager.h"
 
 class SSHModel : public QObject, public Singleton<SSHModel>
 {
     Q_OBJECT
-    Q_PROPERTY(QList<QObject*> sshList READ getSshList NOTIFY sshListChanged)
+    Q_PROPERTY(QList<QObject*> sshList READ getSSHList NOTIFY sshListChanged)
 
 public:
     explicit SSHModel(QObject *parent = nullptr);
 
     Q_INVOKABLE void addSSHList(const QString &hostName, const QString &userName, const QString &password);
 
-    QList<QObject*> getSshList() const;
+    QList<QObject*> getSSHList() const;
 
 signals:
     void sshListChanged();
--- src/ssh/sshmodel.cpp
+++ src/ssh/sshmodel.cpp
@@ -9,19 +9,28 @@ SSHModel::SSHModel(QObject *parent) : QObject(parent)
 
 void SSHModel::addSSHList(const QString &hostName, const QString &userName, const QString &password)
 {
-    SSHData *sshData = new SSHData(hostName, userName, 22, this);
-    sshList.append(sshData);
+    SSHManager sshManager(hostName, userName, password, 22);
 
-    emit sshListChanged();
-    qDebug() << "Added SSH: Host=" << hostName << ", User=" << userName;
+    if (sshManager.connect()) {
+        SSHData *sshData = new SSHData(hostName, userName, 22, this);
+        sshList.append(sshData);
+
+        emit sshListChanged();
+        qDebug() << "Added SSH: Host=" << hostName << ", User=" << userName;
+    }
+    else {
+        qDebug() << "Failed to add SSH: Host=" << hostName << ", User=" << userName;
+    }
 }
 
-QList<QObject*> SSHModel::getSshList() const
+QList<QObject*> SSHModel::getSSHList() const
 {
     QList<QObject*> objectList;
+
     for (auto *sshData : sshList)
     {
         objectList.append(sshData);
     }
+
     return objectList;
 }
